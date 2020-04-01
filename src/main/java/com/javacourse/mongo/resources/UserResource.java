52e4a012303ed7ab8579dfdecfb85e3283fcc2a5
Wagner <wagner.silvac@ctis.com.br>
2020-04-01 18:53:53 -0300

    UserDto

--- src/main/java/com/javacourse/mongo/resources/UserResource.java
+++ src/main/java/com/javacourse/mongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.javacourse.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javacourse.mongo.domain.User;
+import com.javacourse.mongo.dto.UserDTO;
 import com.javacourse.mongo.services.UserService;
 
 @RestController
@@ -19,9 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
