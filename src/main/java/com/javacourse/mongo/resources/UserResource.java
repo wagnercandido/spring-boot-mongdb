86d5fa0104765446bb8322355f4147e0e0f5afd2
Wagner <wagner.silvac@ctis.com.br>
2020-04-01 17:05:58 -0300

    Add User Respository, Service e Resource

--- src/main/java/com/javacourse/mongo/resources/UserResource.java
+++ src/main/java/com/javacourse/mongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.javacourse.mongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javacourse.mongo.domain.User;
+import com.javacourse.mongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
-	@GetMapping()
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User usuario1 = new User("1", "wagner", "wagner@email.com");
-		User usuario2 = new User("2", "candido", "candido@email.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(usuario1, usuario2));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ src/main/java/com/javacourse/mongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.javacourse.mongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacourse.mongo.domain.User;
+import com.javacourse.mongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+}
