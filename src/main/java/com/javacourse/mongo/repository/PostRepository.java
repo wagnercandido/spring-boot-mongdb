8cdabaeb0e30b97eaa1e8a8a207a8e6266bb1675
Wagner <wagner.silvac@ctis.com.br>
2020-04-02 10:51:06 -0300

    Consulta com @Query

--- src/main/java/com/javacourse/mongo/repository/PostRepository.java
+++ src/main/java/com/javacourse/mongo/repository/PostRepository.java
@@ -3,13 +3,17 @@ package com.javacourse.mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.javacourse.mongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> findTitle(String text);
 
 }
