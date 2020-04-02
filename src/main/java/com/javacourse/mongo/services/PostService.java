f064b69e9c85121899bd325be8668a0733700890
Wagner <wagner.silvac@ctis.com.br>
2020-04-02 10:01:37 -0300

    Obter um post por ID

---
+++ src/main/java/com/javacourse/mongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.javacourse.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacourse.mongo.domain.Post;
+import com.javacourse.mongo.repository.PostRepository;
+import com.javacourse.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
